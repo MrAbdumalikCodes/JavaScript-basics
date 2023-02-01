d83b3b9fcc2df35c131de2e892eb73791e0b05f5
Abdumalik Bakhriddinov <abdumalikcodes7753@gmail.com>
2023-02-01 19:21:01 +0500

    Promise

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-var data = {
-    name:'John',
-    age:32,
-    job:true
-}
-
-var user = {
-    isMaried:true
-}
-
-user.__proto__= data
-console.log(user.name);
+//*********** Promise ***********/
+let isMeetingOkay = true
+const twoFriendMeeting = new Promise(
+    // resolve - qanaqadir malumot keldi
+    // rejcet = qanqadir ma'lumot kelmadi
+    function(resolve,reject){ 
+        if(isMeetingOkay){
+            const msg = console.log('Friend meeting');
+            resolve(msg)
+        }else{
+            const errMessage = 'Not Friend meeting!'
+            reject(errMessage)
+        }
+    }
+)
+// then - keyin
+twoFriendMeeting
+.then(res => res)
+.catch(error => console.log(error))
+.finally(() => console.log('Finished'))
