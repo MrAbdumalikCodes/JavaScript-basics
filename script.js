49c831e816a4ab07d6806b932840a7acf2a17629
Abdumalik Bakhriddinov <abdumalikcodes7753@gmail.com>
2023-02-01 11:20:11 +0500

    Full-bind

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const data ={
     name:'John',
     age:23,
     job:'Androind developer',
-    personInfo:function(){
+    personInfo:function(contact){
         console.group(`${this.name} info:`) // Console group bu huddi object ga uxshatib consolega chiqaradi yani group ga olib masala John info: shu kurinishda
-        console.log('My name is ' + this.name + '.' + 'My ' + this.age + ' years old' + '.' + 'My job is ' + this.job)
+        console.log('My name is ' + this.name + '.' + 'My ' + this.age + ' years old' + '.' + 'My job is ' + this.job + '.' +  `My number ${contact}`)
         console.groupEnd()
     }
 }
@@ -14,11 +14,10 @@ const employes = {
     age:18,
     job:'Software engineer'
 }
-const coder = {
-    name:'Jimy',
-    age:25,
-    job:'Graphic designer'
-}
 
-data.personInfo.bind(employes)()
-data.personInfo.bind(coder)()
+const fullInfo = data.personInfo.bind(employes,'+998-91-425-4848')()
+
+//1. const fullInfo = data.personInfo.bind(employes)
+// fullInfo('+998-91-222-7878');
+
+//2.const fullInfo = data.personInfo.bind(employes,'+998-91-425-4848')();
