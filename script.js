480e51835a18954fe8ba90e0a4137843e793fa04
Abdumalik Bakhriddinov <abdumalikcodes7753@gmail.com>
2023-02-01 18:12:28 +0500

    Prototype

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//1)  Funksiya konstruktor
-
-function User(name,id){
-    this.name = name
-    this.id = id
-    this.human = true
+var data = {
+    name:'John',
+    age:32,
+    job:true
 }
-const employ = new User('Jack',1)
-const some = new User('John',21)
-console.log(employ);
-console.log(some);
+
+var user = {
+    isMaried:true
+}
+
+user.__proto__= data
+console.log(user.name);
