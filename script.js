f2e2410f3a7a17076d9707d9e6575ffb852ec36d
Abdumalik Bakhriddinov <abdumalikcodes7753@gmail.com>
2023-02-01 20:55:20 +0500

    POST

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-//*********** Promise ***********/
-let isMeetingOkay = true
-const twoFriendMeeting = new Promise(
-    // resolve - qanaqadir malumot keldi
-    // rejcet = qanqadir ma'lumot kelmadi
-    function(resolve,reject){ 
-        if(isMeetingOkay){
-            const msg = console.log('Friend meeting');
-            resolve(msg)
-        }else{
-            const errMessage = 'Not Friend meeting!'
-            reject(errMessage)
-        }
-    }
-)
-// then - keyin
-twoFriendMeeting
-.then(res => res)
-.catch(error => console.log(error))
-.finally(() => console.log('Finished'))
+//*********** POST ***********/
+fetch('https://jsonplaceholder.typicode.com/posts',{
+    method:"POST",
+    body:JSON.stringify({name:'Lisa'}),
+    headers:{'Content-type' : '/application/json'}
+})
+      .then(response => response.json())
+      .then(json => console.log(json))
